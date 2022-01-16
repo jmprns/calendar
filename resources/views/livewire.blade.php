2305d481bdde3b2ace30e9150dad9d040133f6b5
Jimwell Parinas <jp.pagapulan@gmail.com>
2022-01-16 11:13:59 +0800

    Update livewire.blade.php

--- resources/views/livewire.blade.php
+++ resources/views/livewire.blade.php
@@ -7,7 +7,7 @@
     <meta name="description" content="">
     <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
     <meta name="generator" content="Hugo 0.84.0">
-    <title>Pricing example · Bootstrap v5.0</title>
+    <title>Calendar - Livewire | Bootstrap</title>
 
     <!-- Bootstrap core CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
@@ -64,7 +64,14 @@
         </header>
 
         <main>
-            {{ $slot }}
+            <div class="row">
+                <div class="col-4">
+                    <livewire:event.form />
+                </div>
+                <div class="col-8">
+                    <livewire:event.calendar />
+                </div>
+            </div>
         </main>
 
     </div>
