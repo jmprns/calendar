d442776685b80db3ed63c1d3b013bd03e5d2a3e4
Jimwell Parinas <jp.pagapulan@gmail.com>
2022-01-16 11:45:47 +0800

    Update 2022_01_11_100559_create_events_table.php
    update string to text

--- database/migrations/2022_01_11_100559_create_events_table.php
+++ database/migrations/2022_01_11_100559_create_events_table.php
@@ -15,7 +15,7 @@ class CreateEventsTable extends Migration
     {
         Schema::create('events', function (Blueprint $table) {
             $table->id();
-            $table->string('title');
+            $table->text('title');
             $table->date('start');
             $table->date('end');
             $table->json('days')->nullable();
